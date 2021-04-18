cdafab2988c70dce37a62eb13005b409561a358c
valerie-m <vmac2268@gmail.com>
2021-04-18 01:24:21 -0400

    Publishing my personal website.

---
+++ scripts/scroll.js
@@ -0,0 +1,27 @@
+button = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+    console.log(document.documentElement.scrollTop)
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function topFunction() {
+  var myVar = setInterval(smoothScroll, 0.05);
+  var y=document.documentElement.scrollTop; //document.documentElement.scrollTop retrieves the current position
+
+  function smoothScroll() {
+    if (y > 0) {
+      window.scrollTo(0, y);
+      y = y-25;
+    } else {
+      window.scrollTo(0, 0);
+      clearInterval(myVar);
+    }
+  }
+}
